39bb44c351227b6dac5a2035ae2bcf90123dff4d
İrem Kaya <38073758+iiremkayaa@users.noreply.github.com>
2021-02-21 16:55:35 +0300

    used reactive form to create post

--- src/app/add-post/add.component.ts
+++ src/app/add-post/add.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'add-post',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
-export class AddPostComponent {
+export class AddPostComponent implements OnInit{
+  title:FormControl;
+  body:FormControl;
+  postForm:FormGroup;
+  constructor(){
+
+  }
+  ngOnInit(){
+    this.title=new FormControl('',Validators.required);
+    this.body=new FormControl('',Validators.required);
+    this.postForm=new FormGroup({
+      title:this.title,
+      body:this.body
+    });
+  }
+  sendPost(data:any){
+    console.log(data)
+
+  }
 }
